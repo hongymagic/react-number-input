419cc9a7e0568584c1069b8e8297aa37c32f1964
hongymagic <d.hong@me.com>
2015-02-06 10:20:45 +1100

    tests: add tests for event handlers

--- __tests__/react-number-input-test.js
+++ __tests__/react-number-input-test.js
@@ -1,65 +1,81 @@
 jest.dontMock('../react-number-input');
 
 describe('react-number-input', function () {
-	it('should display formatted number on initial render', function () {
-		var React = require('react/addons');
-		var NumberInput = require('../react-number-input');
-		var TestUtils = React.addons.TestUtils;
+	var React = require('react/addons');
+	var NumberInput = require('../react-number-input');
+	var TestUtils = React.addons.TestUtils;
+	var Simulate = TestUtils.Simulate;
+
+	describe('input#value', function () {
 		var value = 900000;
+		var component;
 
-		// Render
-		var input = TestUtils.renderIntoDocument(
-			<NumberInput value={value} />
-		);
-
-		// Verify that number has been formatted
-		expect(input.getDOMNode().value).toEqual('900,000');
-	});
-
-	it('should display unformatted number when typing', function () {
-		var React = require('react/addons');
-		var NumberInput = require('../react-number-input');
-		var TestUtils = React.addons.TestUtils;
-		var Simulate = TestUtils.Simulate;
-		var value = 900000;
-
-		// Render
-		var input = TestUtils.renderIntoDocument(
-			<NumberInput value={value} />
-		);
-
-		// Change the number
-		Simulate.change(input.getDOMNode(), {
-			target: {
-				value: '1000000'
-			}
+		beforeEach(function () {
+			component = TestUtils.renderIntoDocument(
+				<NumberInput value={value} />
+			);
 		});
 
-		// Verify that number has been formatted
-		expect(input.getDOMNode().value).toEqual('1000000');
+		it('should display formatted value when rendered', function () {
+			// Verify that number has been formatted
+			expect(component.getDOMNode().value).toEqual('900,000');
+		});
+
+		it('should display un-formatted number when editing', function () {
+			// Change the number
+			Simulate.change(component.getDOMNode(), {
+				target: {
+					value: '1000000'
+				}
+			});
+
+			// Verify that number has been formatted
+			expect(component.getDOMNode().value).toEqual('1000000');
+		});
+
+		it('should display formatted number when editing is complete', function () {
+			// Change the number
+			Simulate.change(component.getDOMNode(), {
+				target: {
+					value: '1000000'
+				}
+			});
+			Simulate.blur(component.getDOMNode());
+
+			// Verify that number has been formatted
+			expect(component.getDOMNode().value).toEqual('1,000,000');
+		});
 	});
 
-	it('should display formatted number when changed', function () {
-		var React = require('react/addons');
-		var NumberInput = require('../react-number-input');
-		var TestUtils = React.addons.TestUtils;
-		var Simulate = TestUtils.Simulate;
+	describe('onChange', function () {
 		var value = 900000;
+		var handlers, component;
 
-		// Render
-		var input = TestUtils.renderIntoDocument(
-			<NumberInput value={value} />
-		);
+		beforeEach(function () {
+			handlers = {
+				onChange: function (event) {
+					console.log(event);
+				}
+			};
 
-		// Change the number
-		Simulate.change(input.getDOMNode(), {
-			target: {
-				value: '1000000'
-			}
+			spyOn(handlers, 'onChange');
+			component = TestUtils.renderIntoDocument(
+				<NumberInput value={value} onChange={handlers.onChange} />
+			);
+
+			Simulate.change(component.getDOMNode(), {
+				target: {
+					value: '1000000'
+				}
+			});
 		});
-		Simulate.blur(input.getDOMNode());
 
-		// Verify that number has been formatted
-		expect(input.getDOMNode().value).toEqual('1,000,000');
+		it('should be run when input is changed', function () {
+			expect(handlers.onChange).toHaveBeenCalled();
+		});
+
+		it('should be passed unformatted value', function () {
+			expect(handlers.onChange.mostRecentCall.args[0].target.value).toEqual('1000000');
+		});
 	});
 });
