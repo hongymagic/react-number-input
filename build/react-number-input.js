4efdc17145cb47169067a5016644fa5962314c52
Adam Al-Mutari <aalmutari@infusion.com>
2016-03-03 10:10:42 +0100

    Add file after build

--- build/react-number-input.js
+++ build/react-number-input.js
@@ -72,7 +72,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 	
-	var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+	var _get = function get(_x2, _x3, _x4) { var _again = true; _function: while (_again) { var object = _x2, property = _x3, receiver = _x4; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x2 = parent; _x3 = property; _x4 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 	
 	exports.isNumber = isNumber;
 	exports.toNumeral = toNumeral;
@@ -127,7 +127,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 		// numeral.js converts empty strings into 0 for no reason, so if given
 		// value was not '0' or 0, treat it as null.
-		if (n.value() === 0 && (value !== 0 && value !== '0')) {
+		if (n.value() === 0 && value !== 0 && value !== '0') {
 			return null;
 		}
 	
@@ -293,7 +293,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports['default'] = NumberInput;
 	
 	NumberInput.propTypes = {
-		value: _react.PropTypes.number.isRequired,
+		value: _react.PropTypes.number,
 		type: _react.PropTypes.string,
 		format: _react.PropTypes.string,
 		min: _react.PropTypes.number,
