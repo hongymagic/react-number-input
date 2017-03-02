c85a0c0d613c64b170bafaec935e3676e27daf05
hongymagic <hongymagic+github@me.com>
2017-03-02 11:16:06 +1100

    Add onBlur/onFocus events example to stories

--- stories/index.js
+++ stories/index.js
@@ -5,20 +5,29 @@ import NumberInput from '../index.js';
 storiesOf('NumberInput', module)
 	.add('without any properties', () => (
 		<NumberInput
-			onChange={action('changed')}
+			onChange={action('change')}
 		/>
 	))
 	.add('with value', () => (
 		<NumberInput
 			value={1234567890}
-			onChange={action('changed')}
+			onChange={action('change')}
 		/>
 	))
 	.add('with value and optional decimal format "0,0[.]000"', () => (
 		<NumberInput
 			value={3.1427}
 			format="0,0[.]000"
-			onChange={action('changed')}
+			onChange={action('change')}
 		/>
 	))
+	.add('with events', () => (
+		<NumberInput
+			value={3.1427}
+			format="0,0[.]000"
+			onChange={action('change')}
+			onBlur={action('blur')}
+			onFocus={action('focus')}
+		/>
+	));
 
